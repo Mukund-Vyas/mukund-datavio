15557d1f9be23f60923f3c29c4b9419d73372a78
Mukund-Vyas <mukund39ppsv@gmail.com>
2023-08-17 10:02:49 +0530

    this is second commit

--- backend/router/Data.js
+++ backend/router/Data.js
@@ -1,9 +1,9 @@
 const express =  require('express');
-const axios =  require('axios');
-const cheerio =  require('cheerio');
-const authUser =  require('../middleware/Authentication');
-const Data =  require('../models/Data');
-
+const axios =  require('axios'); 
+const cheerio =  require('cheerio'); // fro scraping data
+const authUser =  require('../middleware/Authentication'); // for authentication
+const Data =  require('../models/Data'); // mongoDB schema
+ 
 const router = express.Router();
 
 router.post('/scrape',authUser,async(req,res)=>{
@@ -16,16 +16,17 @@ router.post('/scrape',authUser,async(req,res)=>{
             return res.status(200).json({msg:"data found",data});
         }else{
             const response = await axios.get(url);
-            const $ = cheerio.load(response.data);
+            const $ = cheerio.load(response.data); // fro scrapping data
             
             const title = $('.B_NuCI').text();
             const price = $('._30jeq3 _16Jk6d').text(); 
             const description = $('._1mXcCf RmoJUa').text();
             const rating_review = $('._2_R_DZ').text();
             const rating = $('._3LWZlK').text().trim();
-            const media = $('._3GnUWp').text();
-            // const mediaCount = media.find('._20Gt85 _1Y_A6W').length();
-            console.log({title,
+            const media = $('ul._3GnUWp');
+            const mediaCount = media.children('li._20Gt85 _1Y_A6W').length();
+            console.log({
+                title,
                 price,
                 description,
                 rating_review,
@@ -37,8 +38,8 @@ router.post('/scrape',authUser,async(req,res)=>{
                 price,
                 description,
                 rating_review,
-                rating,            
-                url
+                rating, 
+                mediaCount
             });
             await data1.save();
             res.status(202).json({msg:'Data Scraped and saved',data1});
